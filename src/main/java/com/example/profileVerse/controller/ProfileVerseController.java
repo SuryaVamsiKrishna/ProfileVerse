b9da881cb7a4959b6e9ca991191bb26e29a5137a
SuryaVamsiKrishna <svkpulapa@gmail.com>
2024-11-07 12:40:09 -0800

    Second

--- src/main/java/com/example/profileVerse/controller/ProfileVerseController.java
+++ src/main/java/com/example/profileVerse/controller/ProfileVerseController.java
@@ -1,5 +1,16 @@
 package com.example.profileVerse.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class ProfileVerseController {
 
+	@GetMapping("/")
+    public String home() {
+        return "Welcome to ProfileVerse!";
+	}
+
 }
+
+
